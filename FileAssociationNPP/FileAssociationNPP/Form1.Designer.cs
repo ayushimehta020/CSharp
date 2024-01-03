9a060887eec5923f5fd610ab721fc8502cdc956d
Ayushi Mehta <109412787+ayushimehta020@users.noreply.github.com>
2024-01-03 15:18:17 +0530

    Add files via upload

---
+++ FileAssociationNPP/FileAssociationNPP/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace FileAssociationNPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(153, 238);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(171, 35);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(153, 238);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(421, 35);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(153, 238);
+            this.listBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(340, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(340, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(248, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 33);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Supported extensions:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(418, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Registered extensions:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(586, 354);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FileAssociationNPP/FileAssociationNPP/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileAssociationNPP
+{
+    public partial class Form1 : Form
+    {
+        private string[] supportedExtensions = new string[] { "Notepad", "ms ini/inf", "c, c++, objc", "java, c#, pascal", "web script", "public script", "property script", "fortran, TeX, SQL", "misc" };
+        private string[] notepad = new string[] { ".txt", ".log" };
+        private string[] msIniInf = new string[] { ".ini", ".inf" };
+        private string[] cCppObjc = new string[] { ".h", ".hh", ".hpp", ".hxx", ".c", ".cpp", ".cxx", ".cc", ".m", ".mm", ".vcxproj", ".vcproj", ".props", ".vsprops", ".manifest" };
+        private string[] javaCsPascal = new string[] { ".java", ".cs", ".pas", ".pp", ".inc" };
+        private string[] webScript = new string[] { ".html", ".htm", ".shtml", ".shtm", ".hta", ".asp", ".aspx", ".css", ".js", ".json", ".jsm", ".jsp", ".php", ".php3", ".php4", ".php5", ".phps", ".phpt", ".phtml", ".xml", ".xhtml", ".xht", ".xul", ".kml", ".xaml", ".xsml" };
+        private string[] publicScript = new string[] { ".sh", ".bsh", ".bash", ".bat", ".cmd", ".msi", ".msh", ".lua", ".pl", ".pm", ".py" };
+        private string[] propertyScript = new string[] { ".rc", ".as", ".mx", ".vb", ".vbs" };
+        private string[] fortranTexSql = new string[] { ".f", ".for", ".f90", ".f95", ".f2k", ".tex", ".sql" };
+        private string[] misc = new string[] { ".nfo", ".mak" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string supported in supportedExtensions)
+            {
+                listBox1.Items.Add(supported);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            if (listBox1.Text == "Notepad")
+            {
+                display(notepad);
+            }
+            else if (listBox1.Text == "ms ini/inf")
+            {
+                display(msIniInf);
+            }
+            else if (listBox1.Text == "c, c++, objc")
+            {
+                display(cCppObjc);
+            }
+            else if (listBox1.Text == "java, c#, pascal")
+            {
+                display(javaCsPascal);
+            }
+            else if (listBox1.Text == "web script")
+            {
+                display(webScript);
+            }
+            else if (listBox1.Text == "public script")
+            {
+                display(publicScript);
+            }
+            else if (listBox1.Text == "property script")
+            {
+                display(propertyScript);
+            }
+            else if (listBox1.Text == "fortran, TeX, SQL")
+            {
+                display(fortranTexSql);
+            }
+            else if (listBox1.Text == "misc")
+            {
+                display(misc);
+            }
+        }
+
+        private string display(string[] res)
+        {
+            foreach (string res1 in res)
+            {
+                return Convert.ToString(listBox2.Items.Add(res));
+            }
+        }
+    }
+}
